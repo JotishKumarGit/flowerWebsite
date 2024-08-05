d6d10c9d50e7e18b4023ccc2be2180250b8d4079
JotishKumarGit <jotishk649@gmail.com>
2024-08-05 18:24:09 +0530

    Changes

---
+++ app.js
@@ -0,0 +1,27 @@
+ 
+ // For dropdown 
+$(document).ready(function () {
+    $('.dropdown-submenu a.test').on("click", function (e) {
+        $(this).next('ul').toggle();
+        e.stopPropagation();
+        e.preventDefault();
+    });
+});
+
+// FOr owl crousel 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
